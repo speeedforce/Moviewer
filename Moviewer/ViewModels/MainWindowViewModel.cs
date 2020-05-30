b7297448423f7b6c3565650f20caa211dcabb67e
speedforce <zstanislav98@gmail.com>
2020-05-30 20:13:29 +0300

    Added button for navigation.

--- Moviewer/ViewModels/MainWindowViewModel.cs
+++ Moviewer/ViewModels/MainWindowViewModel.cs
@@ -14,14 +14,49 @@ namespace Moviewer.ViewModels
             InitializationNotifier = NotifyTaskCompletion.Create(InitializeAsync());
         }
 
-        public INotifyTaskCompletion InitializationNotifier { get; private set; }
+        private INotifyTaskCompletion _initializationNotifier;
+        public INotifyTaskCompletion InitializationNotifier
+        {
+            get => _initializationNotifier;
+            private set
+            {
+                _initializationNotifier = value;
+                OnPropertyChanged();
+            }
+        }
 
         private async Task InitializeAsync()
         {
+            Page = 1;
             _movieService = new MovieService();
-            var data = await _movieService.GetMovies(1);
+            await LoadData(Page);
+        }
+
+        public async Task LoadData(int page)
+        {
+            var data = await _movieService.GetMovies(page);
             await Task.Delay(1000);
             MyItems = new ObservableCollection<Movie>(data);
+        }
+
+        public void NextPage()
+        {
+            if (_page+1 > 10)
+                Page = 1;  
+            else
+                Page = _page + 1;
+            
+            InitializationNotifier = NotifyTaskCompletion.Create(LoadData(Page));
+        }
+
+        public void PrevPage()
+        {
+            if (1 > _page - 1)
+                Page = 10;
+            else
+                Page = _page - 1;
+
+            InitializationNotifier = NotifyTaskCompletion.Create(LoadData(Page));
         }
 
         private ObservableCollection<Movie> _myItems;
@@ -35,6 +70,17 @@ namespace Moviewer.ViewModels
                     _myItems = value;
                     OnPropertyChanged();
                 }
+            }
+        }
+
+        private int _page;
+        public int Page
+        {
+            get => _page;
+            set
+            {
+                _page = value;
+                OnPropertyChanged();
             }
         }
     }
