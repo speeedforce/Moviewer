c84f9d1864f2b3ab6491733b2c46aae2d19a053e
speedforce <zstanislav98@gmail.com>
2020-05-30 20:04:40 +0300

    Use model in viewmodel and view. Show movies.

--- Moviewer/ViewModels/MainWindowViewModel.cs
+++ Moviewer/ViewModels/MainWindowViewModel.cs
@@ -1,7 +1,41 @@
-﻿namespace Moviewer.ViewModels
+﻿using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using Moviewer.Models;
+using Moviewer.Services;
+using Nito.AsyncEx;
+
+namespace Moviewer.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
-        public string Text => "Welcome to Avalonia";
+        private MovieService _movieService;
+        public MainWindowViewModel()
+        {
+            InitializationNotifier = NotifyTaskCompletion.Create(InitializeAsync());
+        }
+
+        public INotifyTaskCompletion InitializationNotifier { get; private set; }
+
+        private async Task InitializeAsync()
+        {
+            _movieService = new MovieService();
+            var data = await _movieService.GetMovies(1);
+            await Task.Delay(1000);
+            MyItems = new ObservableCollection<Movie>(data);
+        }
+
+        private ObservableCollection<Movie> _myItems;
+        public ObservableCollection<Movie> MyItems
+        {
+            get => _myItems;
+            set
+            {
+                if (value != null)
+                {
+                    _myItems = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
     }
 }
